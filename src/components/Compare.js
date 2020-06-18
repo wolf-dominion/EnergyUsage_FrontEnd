05f141d1b3d3cce07fce334550177284c13329fd
wolf-dominion <hannah_drury@yahoo.com>
2020-06-18 11:23:39 -0600

    energy use charts a and b display and reset

--- src/components/Compare.js
+++ src/components/Compare.js
@@ -8,11 +8,28 @@ class Compare extends Component {
         this.props.setB(undefined)
     }
 
+    generateCharts = () => {
+        if (this.props.chartA && !this.props.chartB){
+            return <EnergyChart chartA={this.props.chartA}/>
+        }
+        if (this.props.chartA && this.props.chartB){
+            return <EnergyChart chartA={this.props.chartA} chartB={this.props.chartB}/>
+        }
+        if (!this.props.chartA && !this.props.chartB) {
+            return <p>No data selected</p>
+        }
+    }
+
     render(){
+
+        let genChart = this.generateCharts()
+
         return(
             <div className="chart-container">
                 <h2>Energy Use Comparison</h2>
                 {/* {this.state.chartA.length > 0 ? <EnergyChart chartA={this.state.chartA} /> : <p>No charts selected</p>} */}
+                {/* {this.props.chartA ? <EnergyChart /> : null} */}
+                {genChart}
                 <button onClick={(e)=> this.handleReset(e)} >Reset</button>
             </div>
         )
--- src/components/EnergyChart.js
+++ src/components/EnergyChart.js
@@ -1,15 +1,74 @@
 import React, { Component } from 'react'
+import {Bar} from 'react-chartjs-2'
 
-class EnergyChart extends Component {
+function EnergyChart (props) {
 
-    render(){
 
-        return(
-            <div>
-                <p>I'm a chart</p>
-            </div>
-        )
+    function determineDataSet(){
+        if(!props.chartB){
+            const dataForA = {
+                labels: ['Avg', 'Max', 'Min'],
+                datasets: [
+                  {
+                    label: `${props.chartA.energyInfo} in ${props.chartA.zip}`,
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data: [props.chartA.avg, props.chartA.max, props.chartA.min]
+                  }
+                ]
+              }
+            return dataForA
+        }
+        if(props.chartB){
+            const dataForCompare = {
+                labels: ['Avg', 'Max', 'Min'],
+                datasets: [
+                  {
+                    label: `${props.chartA.energyInfo} in ${props.chartA.zip}`,
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data: [props.chartA.avg, props.chartA.max, props.chartA.min]
+                  }, 
+                  {
+                    label: `${props.chartB.energyInfo} in ${props.chartB.zip}`,
+                    backgroundColor: 'rgba(99, 132, 0, 0.6)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.9)',
+                    hoverBorderColor: 'rgba(99, 132, 0, 1)',
+                    data: [props.chartB.avg, props.chartB.max, props.chartB.min] 
+                  }
+                ]
+              }
+            return dataForCompare
+        }
     }
+
+    let finalData = determineDataSet()
+      
+      
+
+    function display() {
+        if (props.chartA && !props.chartB){
+            return <Bar data={finalData} options={{maintainAspectRatio: true}}/>
+        }
+        if (props.chartA && props.chartB){
+            return <Bar data={finalData} options={{maintainAspectRatio: true}}/>
+        }
+    }
+
+    let dis = display()
+    return(
+        <div className='chart-div'>
+            {dis}
+        </div>
+    )
 }
 
 export default EnergyChart
